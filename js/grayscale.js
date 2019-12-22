c3acd2ff15fe5179e0eea3347887eee8692c7082
Juan Sebastian Cardona <js.cardona64@gmail.com>
2019-12-22 14:20:49 -0500

    change logo when scrolling

--- js/grayscale.js
+++ js/grayscale.js
@@ -26,6 +26,20 @@
     offset: 100
   });
 
+  $(document).ready(function() {
+    $(window).scroll( function(){
+      $('.fadeIn').each( function(i){
+
+        var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+        if( bottom_of_window > bottom_of_element ){
+          $(this).animate({'opacity':'1'},1000);
+        }
+
+      });
+    });
+  });
   // Collapse Navbar
   var navbarCollapse = function() {
     if ($("#mainNav").offset().top > 100) {
